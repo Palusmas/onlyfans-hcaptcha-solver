8aa7efc9372f629fa410639d145bc03b0a6aed46
Palusmas <148269261+Palusmas@users.noreply.github.com>
2023-11-08 17:07:26 +0100

    Update and rename onlyfans_sitekey1.py to onlyfans_solver.py

--- onlyfans_solver.py
+++ onlyfans_solver.py
@@ -4,7 +4,7 @@ import requests
 
 # Consider using environment variables for sensitive information
 PROXY = "http://username:password@ip:port"
-capsolver.api_key = ""
+capsolver.api_key = "https://capsolver.com key"
 PAGE_URL = "https://onlyfans.com/"
 PAGE_KEY = "314ec50a-c08a-4c0a-a5c4-4ed4c7ed5aff" # or 7c8456cf-fb4e-48fc-a054-d97bc7765634
 
